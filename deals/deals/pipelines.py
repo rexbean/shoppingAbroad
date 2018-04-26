85fe2f82eafb77d86809bcc8f72e6d84d94e3be0
root <root@vultr.guest>
2018-04-27 05:54:08 +0800

    DB insertion done

--- deals/deals/pipelines.py
+++ deals/deals/pipelines.py
@@ -21,13 +21,12 @@ class DBPipeline(object):
                 charset='utf8',
                 use_unicode=True)
             # cursor is used to do the operation of database
-            print('connected')
+            print('DB has been connected')
             self.cursor = self.connect.cursor();
         except Exception as e:
             print(e)
 
     def process_item(self, item, spider):
-        print('insert begin')
         try:
             # insert data
             self.cursor.execute(
@@ -37,7 +36,7 @@ class DBPipeline(object):
                  item['link'],
                  item['pic'],
                  item['hotness'],
-                 item['editor_recommond'],
+                 item['editor'],
                  item['postTime'],
                  item['description'],
                  item['price'],
@@ -45,7 +44,6 @@ class DBPipeline(object):
 
             # commit
             self.connect.commit()
-            print('insert it')
         except Exception as error:
             # print error
             print(error)
